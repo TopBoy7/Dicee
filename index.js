26776fe7e8bedd8914cadf137f8d2229717e1035
Joseph Okefe <108914484+TopBoy7@users.noreply.github.com>
2023-07-02 05:27:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,100 @@
-
-// Generate random numbers
-var randomPick1 = Math.floor(Math.random() * 6) + 1;
-var randomPick2 = Math.floor(Math.random() * 6) + 1;
-
-// images
-var imageSource1 = "dice" + randomPick1 + ".png"
-var image1 = "images/" + imageSource1;
-
-var imageSource2 = "dice" + randomPick2 + ".png"
-var image2 = "images/" + imageSource2;
-
-//Update Dice1
-var update1 = document.querySelectorAll("img")[0]
-
-update1.setAttribute("src", image1)
-
-
-//Update Dice2
-var update2 = document.querySelectorAll("img")[1]
-
-update2.setAttribute("src", image2)
-
-
-//Update Text
-if (randomPick1 > randomPick2) {
-  document.querySelector("h1").textContent = "Player 1 Wins"
-} 
-else if (randomPick2 > randomPick1){
-  document.querySelector("h1").textContent = "Player 2 Wins"
+// Die 1
+var die1 = [];
+for (let i = 1; i < 7; i++) {
+  die1.push("images/dice"+[i]+".png");
 }
-else {
-  document.querySelector("h1").textContent = "Draw"
+
+// Die 2
+var die2 = [];
+for (let i = 1; i < 7; i++) {
+  die2.push("images/dice"+[i]+".png");
+}
+
+//Generate random numbers for die1
+var number1 = Math.random();
+var n1 = Math.floor(number1 * 6);
+
+//Generate random number for die2
+var number2 = Math.random();
+var n2 = Math.floor(number2 * 6);
+
+//setting the attribute randomly
+document.querySelector(".img1").setAttribute("src", die1[n1]);
+
+//setting the attribute randomly
+document.querySelector(".img2").setAttribute("src", die1[n2]);
+
+//Updating the header
+if (n1 > n2) {
+  document.querySelector("h1").textContent = "Player 1 wins!"; 
+}else if (n2 > n1) {
+  document.querySelector("h1").textContent = "Player 2 wins!";
+} else {
+  document.querySelector("h1").textContent = "Draw!";
+}// Die 1
+var die1 = [];
+for (let i = 1; i < 7; i++) {
+  die1.push("images/dice"+[i]+".png");
+}
+
+// Die 2
+var die2 = [];
+for (let i = 1; i < 7; i++) {
+  die2.push("images/dice"+[i]+".png");
+}
+
+//Generate random numbers for die1
+var number1 = Math.random();
+var n1 = Math.floor(number1 * 6);
+
+//Generate random number for die2
+var number2 = Math.random();
+var n2 = Math.floor(number2 * 6);
+
+//setting the attribute randomly
+document.querySelector(".img1").setAttribute("src", die1[n1]);
+
+//setting the attribute randomly
+document.querySelector(".img2").setAttribute("src", die1[n2]);
+
+//Updating the header
+if (n1 > n2) {
+  document.querySelector("h1").textContent = "Player 1 wins!"; 
+}else if (n2 > n1) {
+  document.querySelector("h1").textContent = "Player 2 wins!";
+} else {
+  document.querySelector("h1").textContent = "Draw!";
+}// Die 1
+var die1 = [];
+for (let i = 1; i < 7; i++) {
+  die1.push("images/dice"+[i]+".png");
+}
+
+// Die 2
+var die2 = [];
+for (let i = 1; i < 7; i++) {
+  die2.push("images/dice"+[i]+".png");
+}
+
+//Generate random numbers for die1
+var number1 = Math.random();
+var n1 = Math.floor(number1 * 6);
+
+//Generate random number for die2
+var number2 = Math.random();
+var n2 = Math.floor(number2 * 6);
+
+//setting the attribute randomly
+document.querySelector(".img1").setAttribute("src", die1[n1]);
+
+//setting the attribute randomly
+document.querySelector(".img2").setAttribute("src", die1[n2]);
+
+//Updating the header
+if (n1 > n2) {
+  document.querySelector("h1").textContent = "Player 1 wins!"; 
+}else if (n2 > n1) {
+  document.querySelector("h1").textContent = "Player 2 wins!";
+} else {
+  document.querySelector("h1").textContent = "Draw!";
 }
